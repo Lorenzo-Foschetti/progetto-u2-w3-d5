88630e37f66548d83d65ed36d246a3f9d04fa3d2
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-04-12 16:51:22 +0200

    comm 2

--- details.js
+++ details.js
@@ -1,1 +1,43 @@
+const p = new URLSearchParams(window.location.search);
+const id = p.get("appId");
+const URL = "https://striveschool-api.herokuapp.com/api/product/" + id;
+console.log(id);
 
+window.addEventListener("DOMContentLoaded", () => {
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZThmZTdmMzA0NjAwMWFlNTlmNjQiLCJpYXQiOjE3MTI5MDg1NDIsImV4cCI6MTcxNDExODE0Mn0.HTyHbwm_i3j_HHEEo7RLX_c4Ig9ZFkUweIyzetZx1jE",
+    },
+  })
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("Error ");
+      }
+    })
+    .then((products) => {
+      const container = document.getElementById("container");
+
+      console.log(products);
+      const div = document.createElement("div");
+
+      div.innerHTML = `  
+    <img src=${products.imageUrl} alt="...">
+    <div> 
+    <h3>${products.name}</h3> 
+    <p>${products.description}</p>
+    <p>€${products.price}</p> 
+    <p>€${products._id}</p>
+ 
+  
+    </div> `;
+
+      container.appendChild(div);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
